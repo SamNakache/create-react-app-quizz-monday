97399eaae92fb1e46ba7ede74b041dcd00095c73
Samuel Nakache <samuel.nakache93@gmail.com>
2022-04-03 23:27:43 +0300

    Show question + statistics
    Need to do 50/50, shuffle, time per question, API

--- quiz-m/src/index.js
+++ quiz-m/src/index.js
@@ -6,9 +6,21 @@ import App from './App';
 document.addEventListener('readystatechange', event => { 
   if (event.target.readyState === "complete") {
     countDown();
+    //getData();
     NextQuestion();
   }
 });
+
+document.getElementById("option-one-label").addEventListener("click", function(){ checkAnswer("optionA"); });
+document.getElementById("option-two-label").addEventListener("click", function(){ checkAnswer("optionB"); });
+document.getElementById("option-three-label").addEventListener("click", function(){ checkAnswer("optionC"); });
+document.getElementById("option-four-label").addEventListener("click", function(){ checkAnswer("optionD"); });
+
+document.getElementById("continue").addEventListener("click", function(){ newGame() });
+
+function test(index){
+  alert(index)
+}
 
 
 const questions = [
@@ -19,32 +31,62 @@ const questions = [
       optionC: "5 days",
       optionD: "7 days",
       correctOption: "optionD"
-  }
+  },
+  {
+    question: "How old am I ?",
+    optionA: "20",
+    optionB: "29",
+    optionC: "15",
+    optionD: "50",
+    correctOption: "optionB"
+}
 ]
 
-let questionNumber = 1 //holds the current question number
 let playerScore = 0  //holds the player score
-let wrongAttempt = 0 //amount of wrong answers picked by player
+document.getElementById("player-score").innerHTML = playerScore;
+let wrongAnswers = 0 //amount of wrong answers picked by player
+let rightAnswers = 0
 let indexNumber = 0 //will be used in displaying next question
+let currentQuestion = questions[indexNumber]
+let bestScore = 0;
+document.getElementById("best-score").innerHTML = playerScore;
+const dataQuestions = [];
 
 // function for displaying next question in the array to dom
 //also handles displaying players and quiz information to dom
 function NextQuestion() {
   //handleQuestions()
-  const currentQuestion = questions[0]
   //document.getElementById("player-score").innerHTML = playerScore
   document.getElementById("display-question").innerHTML = currentQuestion.question;
   document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
   document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
   document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
   document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+  
+}
 
-  //document.getElementById("remaining-time").innerHTML = new Date.getTime();
+function checkAnswer(option){
+  if (option == currentQuestion.correctOption){
+    playerScore += 10;
+    document.getElementById("player-score").innerHTML = playerScore
+    rightAnswers++;
+  }
+  else{
+    playerScore -=5;
+    document.getElementById("player-score").innerHTML = playerScore
+    wrongAnswers++;
+  }
+    
+    if (indexNumber<questions.length){
+      indexNumber++
+    currentQuestion = questions[indexNumber];
+    NextQuestion();
+    } 
 }
 
 
 function countDown(){
-  var countDownDate = addMinutes(new Date().getTime(), 1)
+  var countDownDate = addMinutes(new Date().getTime(), 0.5)
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -68,7 +110,7 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("remaining-time").innerHTML = "EXPIRED";
+    endOfGame();
   }
 }, 1000);
 }
@@ -76,130 +118,93 @@ var x = setInterval(function() {
 function addMinutes(date, minutes) {
   return new Date(date + minutes*60000);
 }
-/*
 
-function checkForAnswer() {
-  const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
-  const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
-  const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
-  let correctOption = null
+function endOfGame(){
+document.getElementById("remaining-time").innerHTML = "EXPIRED";
+    document.getElementById('score-modal').style.display = 'flex';
+    document.getElementById('wrong-answers').innerHTML = wrongAnswers;
+    document.getElementById('right-answers').innerHTML = rightAnswers;
+    document.getElementById('score').innerHTML = playerScore;
 
-  options.forEach((option) => {
-      if (option.value === currentQuestionAnswer) {
-          //get's correct's radio input with correct answer
-          correctOption = option.labels[0].id
-      }
-  })
+}
 
-  //checking to make sure a radio input has been checked or an option being chosen
-  if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
-      document.getElementById('option-modal').style.display = "flex"
+function newGame(){
+  countDown();
+    //getData();
+  eraseEverything()
+    NextQuestion();
+}
+
+function eraseEverything(){
+  if (playerScore>bestScore){
+    bestScore = playerScore
+    document.getElementById("best-score").innerHTML = playerScore;
   }
-
-  //checking if checked radio button is same as answer
-  options.forEach((option) => {
-      if (option.checked === true && option.value === currentQuestionAnswer) {
-          document.getElementById(correctOption).style.backgroundColor = "green"
-          playerScore++ //adding to player's score
-          indexNumber++ //adding 1 to index so has to display next question..
-          //set to delay question number till when next question loads
-          setTimeout(() => {
-              questionNumber++
-          }, 1000)
-      }
-
-      else if (option.checked && option.value !== currentQuestionAnswer) {
-          const wrongLabelId = option.labels[0].id
-          document.getElementById(wrongLabelId).style.backgroundColor = "red"
-          document.getElementById(correctOption).style.backgroundColor = "green"
-          wrongAttempt++ //adds 1 to wrong attempts 
-          indexNumber++
-          //set to delay question number till when next question loads
-          setTimeout(() => {
-              questionNumber++
-          }, 1000)
-      }
-  })
+    
+  document.getElementById('score-modal').style.display = 'none';
+  playerScore = 0  //holds the player score
+document.getElementById("player-score").innerHTML = playerScore;
+wrongAnswers = 0 //amount of wrong answers picked by player
+rightAnswers = 0
+indexNumber = 0 //will be used in displaying next question
+currentQuestion = questions[indexNumber]
 }
 
+/*function getData(){
 
+  var getJSON = function(url, callback) {
 
-//called when the next button is called
-function handleNextQuestion() {
-  checkForAnswer() //check if player picked right or wrong option
-  unCheckRadioButtons()
-  //delays next question displaying for a second just for some effects so questions don't rush in on player
-  setTimeout(() => {
-      if (indexNumber <= 9) {
-//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
-          NextQuestion(indexNumber)
-      }
-      else {
-          handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
-      }
-      resetOptionBackground()
-  }, 1000);
-}
+    var xmlhttprequest = new XMLHttpRequest();
+    xmlhttprequest.open('GET', url, true);
+    xmlhttprequest.responseType = 'json';
 
-//sets options background back to null after display the right/wrong colors
-function resetOptionBackground() {
-  const options = document.getElementsByName("option");
-  options.forEach((option) => {
-      document.getElementById(option.labels[0].id).style.backgroundColor = ""
-  })
-}
+    xmlhttprequest.onload = function() {
 
-// unchecking all radio buttons for next question(can be done with map or foreach loop also)
-function unCheckRadioButtons() {
-  const options = document.getElementsByName("option");
-  for (let i = 0; i < options.length; i++) {
-      options[i].checked = false;
-  }
-}
+        var status = xmlhttprequest.status;
 
-// function for when all questions being answered
-function handleEndGame() {
-  let remark = null
-  let remarkColor = null
+        if (status == 200) {
+            callback(null, xmlhttprequest.response);
+        } else {
+            callback(status, xmlhttprequest.response);
+        }
+    };
 
-  // condition check for player remark and remark color
-  if (playerScore <= 3) {
-      remark = "Bad Grades, Keep Practicing."
-      remarkColor = "red"
-  }
-  else if (playerScore >= 4 && playerScore < 7) {
-      remark = "Average Grades, You can do better."
-      remarkColor = "orange"
-  }
-  else if (playerScore >= 7) {
-      remark = "Excellent, Keep the good work going."
-      remarkColor = "green"
-  }
-  const playerGrade = (playerScore / 10) * 100
+    xmlhttprequest.send();
+};
 
-  //data to display to score board
-  document.getElementById('remarks').innerHTML = remark
-  document.getElementById('remarks').style.color = remarkColor
-  document.getElementById('grade-percentage').innerHTML = playerGrade
-  document.getElementById('wrong-answers').innerHTML = wrongAttempt
-  document.getElementById('right-answers').innerHTML = playerScore
-  document.getElementById('score-modal').style.display = "flex"
+getJSON('https://opentdb.com/api.php?amount=100',  function(err, data) {
 
-}
+    if (err != null) {
+        console.error(err);
+    } else {
 
-//closes score modal, resets game and reshuffles questions
-function closeScoreModal() {
-  questionNumber = 1
-  playerScore = 0
-  wrongAttempt = 0
-  indexNumber = 0
-  shuffledQuestions = []
-  NextQuestion(indexNumber)
-  document.getElementById('score-modal').style.display = "none"
-}
+      var res = 'category: ${data.result[0].category}';
+      console.log(res.category)
 
-//function to close warning modal
-function closeOptionModal() {
-  document.getElementById('option-modal').style.display = "none"
+       var display = `User_ID: ${data.userId}
+ID: ${data.id}
+Title: ${data.title} 
+Completion_Status: ${data.completed}`;
+    }
+  
+});
+      
+      data.forEach(q => {
+        const newQuestion = {
+          question: q.question,
+          optionA: q.correct_answer,
+          optionB: q.incorrect_answers[0],
+          optionC: q.incorrect_answers[1],
+          optionD: q.incorrect_answers[2],
+          correctOption: q.correct_answer
+        }
+    
+        console.log(newQuestion.question)
+    
+        dataQuestions.push(newQuestion);
+        
+      });
+   
+
 }*/
 
