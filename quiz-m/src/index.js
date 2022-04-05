7cbb35b84e2a6053b583add6cf2b482d38c6c110
Samuel Nakache <samuel.nakache93@gmail.com>
2022-04-06 00:33:52 +0300

    Fix problem with timer, fix problem with end panel
    Add 50/50 option

--- quiz-m/src/index.js
+++ quiz-m/src/index.js
@@ -4,40 +4,40 @@ import './index.css';
 import App from './App';
 
 
-document.getElementById("continue").addEventListener("click", function () { newGame() });
 getData()
-
 
 let playerScore = 0  //holds the player score
 document.getElementById("player-score").innerHTML = playerScore;
 let wrongAnswers = 0 //amount of wrong answers picked by player
 let rightAnswers = 0
 let indexNumber = 0 //will be used in displaying next question
-let currentQuestion
+let currentQuestion;
 let bestScore = 0;
 document.getElementById("best-score").innerHTML = playerScore;
-const dataQuestions = [];
+var dataQuestions = [];
+var cell = document.getElementById("option-one-label")
 setTimeout(() => { countDown(); }, 2000);
+var x;
+var l = 5; 
 
 
 
 // function for displaying next question in the array to dom
 //also handles displaying players and quiz information to dom
 function NextQuestion(index) {
+  removeColor()
   console.log(index)
-  if (index < dataQuestions.length) {
+  if (index < l) {
     indexNumber = index;
     currentQuestion = dataQuestions[indexNumber];
     document.getElementById("display-question").innerHTML = currentQuestion.question;
     document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
-    if (currentQuestion.nIncorrectOptions === 1) {
-      document.getElementById("option-two-label").innerHTML = currentQuestion.optionD;
-      currentQuestion.correctOption = "optionD";
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    if (currentQuestion.nOptions === 2) {
       document.getElementById("option-three-label").style.display = 'none';
       document.getElementById("option-four-label").style.display = 'none';
     }
     else {
-      document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
       document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
       document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
       document.getElementById("option-three-label").style.display = 'flex';
@@ -60,21 +60,46 @@ function checkAnswer(option) {
     document.getElementById("player-score").innerHTML = playerScore
     wrongAnswers++;
   }
+  colorRightAnswer()
+  
 }
 
 
+function colorRightAnswer(){
+  switch(currentQuestion.correctOption){
+    case 'optionA':
+      cell = document.getElementById("option-one-label")
+      break;
+    case 'optionB':
+      cell= document.getElementById("option-two-label")
+      break;
+    case 'optionC':
+      cell= document.getElementById("option-three-label")
+      break;
+    case 'optionD':
+      cell= document.getElementById("option-four-label")
+      break;
+    default:
+  }
+  cell.style.backgroundColor = 'green'
+}
 
+function removeColor(){
+  cell.style.backgroundColor = 'lightgray'
+}
 
 function countDown() {
-  NextQuestion(indexNumber)
+  
+    NextQuestion(indexNumber);
+    var seconds = 14;
 
-  var seconds = 9;
+  
 
   // Update the count down every 1 second
-  var x = setInterval(function () {
+  x = setInterval(function () {
 
     // Display the result in the element 
-    addColor(9 / seconds)
+    addColor(14 / seconds)
     document.getElementById("remaining-time").innerHTML = seconds;
 
 
@@ -82,25 +107,25 @@ function countDown() {
     if (seconds < 1) {
       stopInterval()
     }
-    if (!(indexNumber < dataQuestions.length)) {
+    if (!(indexNumber < l)) {
       clearInterval(x)
+      document.getElementById("remaining-time").innerHTML = "END";
     }
     seconds--;
   }, 1000);
-
-  function stopInterval() {
-    clearInterval(x);
-    indexNumber++;
-    countDown();
-  }
-
-
-
-  document.getElementById("option-one-label").addEventListener("click", function () { checkAnswer("optionA"); stopInterval() });
-  document.getElementById("option-two-label").addEventListener("click", function () { checkAnswer("optionB"); stopInterval() });
-  document.getElementById("option-three-label").addEventListener("click", function () { checkAnswer("optionC"); stopInterval() });
-  document.getElementById("option-four-label").addEventListener("click", function () { checkAnswer("optionD"); stopInterval() });
+  
 }
+function stopInterval(){
+      clearInterval(x);
+      indexNumber++;
+        countDown();
+    }
+
+
+document.getElementById("option-one-label").addEventListener("click", function () { checkAnswer("optionA"); setTimeout(() => { stopInterval(); }, 1000); });
+document.getElementById("option-two-label").addEventListener("click", function () { checkAnswer("optionB"); setTimeout(() => { stopInterval(); }, 1000); });
+document.getElementById("option-three-label").addEventListener("click", function () { checkAnswer("optionC"); setTimeout(() => { stopInterval(); }, 1000); });
+document.getElementById("option-four-label").addEventListener("click", function () { checkAnswer("optionD"); setTimeout(() => { stopInterval(); }, 1000); });
 
 function addColor(result) {
   if (result >= 3)
@@ -113,7 +138,6 @@ function addColor(result) {
 
 
 function endOfGame() {
-  document.getElementById("remaining-time").innerHTML = "END";
   document.getElementById('score-modal').style.display = 'flex';
   document.getElementById('wrong-answers').innerHTML = wrongAnswers;
   document.getElementById('right-answers').innerHTML = rightAnswers;
@@ -121,18 +145,20 @@ function endOfGame() {
 
 }
 
-function newGame() {
-  eraseEverything()
+//New Game
+document.getElementById("continue").addEventListener("click", function () { 
+  eraseEverything();
   getData();
-  countDown();
-}
+  setTimeout(() => { countDown(); }, 2000);
+});
+
+
 
 function eraseEverything() {
   if (playerScore > bestScore) {
     bestScore = playerScore
     document.getElementById("best-score").innerHTML = playerScore;
   }
-
   document.getElementById('score-modal').style.display = 'none';
   playerScore = 0  //holds the player score
   document.getElementById("player-score").innerHTML = playerScore;
@@ -151,9 +177,9 @@ function getData() {
     .then(function (data) {
       let q = data.results;
       q.forEach(element => {
-        var shuf = element.incorrect_answers.push(element.correct_answer)
+        element.incorrect_answers.push(element.correct_answer)
         shuffleArray(element.incorrect_answers)
-        var cOption = ""
+        var cOption = "";//Will contain the index of the right answer after we shuffled the options
         var c = element.incorrect_answers.indexOf(element.correct_answer)
         switch (c) {
           case 0:
@@ -168,6 +194,7 @@ function getData() {
           case 3:
             cOption = "optionD"
             break;
+          default:
         }
 
         const temp = {
@@ -177,7 +204,7 @@ function getData() {
           optionC: element.incorrect_answers[2],
           optionD: element.incorrect_answers[3],
           correctOption: cOption,
-          nIncorrectOptions: element.incorrect_answers.length
+          nOptions: element.incorrect_answers.length//Will contain the number of options for each question
         }
         dataQuestions.push(temp)
       });
@@ -185,6 +212,7 @@ function getData() {
     .catch(function (error) {
       console.log(error);
     });
+    //l = dataQuestions.length;
 
 }
 
